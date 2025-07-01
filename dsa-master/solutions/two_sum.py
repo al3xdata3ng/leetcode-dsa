e384279912a3cb33693d70f84ac6461d7cb61b1e
Alexandros Vitsas <al3xdata3ng@gmail.com>
2025-07-02 00:07:14 +0300

    chore(format): formatted two sums

--- dsa-master/solutions/two_sum.py
+++ dsa-master/solutions/two_sum.py
@@ -1,6 +1,6 @@
 # problem statement url: https://leetcode.com/problems/two-sum/description/?utm_source=instabyte.io&utm_medium=referral&utm_campaign=interview-master-100
 
-""" 
+"""
 Brute force solution would be a double for loop, which would lead to O(n^2) time complexity.
 
 Intuition for achieving O(n) time complexity:
@@ -15,25 +15,20 @@ Solution inspired from: https://instabyte.io/p/amazon-coding-challenge-netflix-d
 
 from typing import Optional
 
-def two_sums(array: list[int], target: int) -> Optional[list]:
 
+def two_sums(array: list[int], target: int) -> Optional[list]:
     idx_int_dict = {}
     for i, integer in enumerate(array):
-        complement = target - integer # this would be the solution of our equation
+        complement = target - integer  # this would be the solution of our equation
 
         if complement in idx_int_dict:
-
             return [idx_int_dict[complement], i]
-    
+
         idx_int_dict[integer] = i
 
-if __name__ == "__main__":
 
-    nums = [3,3]
+if __name__ == "__main__":
+    nums = [3, 3]
     target = 6
 
     print(two_sums(array=nums, target=target))
-
-
-
-
