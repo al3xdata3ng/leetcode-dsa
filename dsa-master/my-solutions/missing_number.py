8b74496640279ecd07275f6ae71cf93417afa4a1
Alexandros Vitsas <al3xdata3ng@gmail.com>
2025-07-27 15:15:36 +0300

    feat(missing-number: added warning message

--- dsa-master/my-solutions/missing_number.py
+++ dsa-master/my-solutions/missing_number.py
@@ -1,3 +1,5 @@
+# WARNING: works only if integers are starting from 1!
+
 def missing_number(array: list[int]) -> int | None:
     n = len(array) + 1 
     expected_sum: int = int(n*(n + 1) / 2)
@@ -8,6 +10,6 @@ def missing_number(array: list[int]) -> int | None:
 
 if __name__ == "__main__":
 
-    array = [3, 5, 1, 4]
+    array = [152, 164, 169, 153, 174, 157, 163, 168, 173, 154, 159, 172, 166, 167, 155, 162, 170, 176, 160, 165, 175, 178, 161, 171, 158, 180, 177, 156, 181, 183, 182, 186, 184, 185, 187, 188, 189, 190, 191, 192, 194, 195, 196, 197, 198, 193, 199, 200, 201, 202]
 
     print(missing_number(array=array))
