7b23d9dbb5b24d1458a1ff3a835598d895c04d00
Alexandros Vitsas <al3xdata3ng@gmail.com>
2025-11-22 21:12:42 +0200

    refactor(two-pointers): code duplication removal

--- patterns/two-pointers/two_sum_2.py
+++ patterns/two-pointers/two_sum_2.py
@@ -9,11 +9,14 @@ def twoSum(numbers: list[int], target: int) -> list[int]:
     right = len(numbers) - 1
 
     while left < right:
-        if numbers[left] + numbers[right] == target:
+
+        sum_ = numbers[left] + numbers[right]
+        
+        if sum_ == target:
             return [left + 1, right + 1]
-        elif numbers[left] + numbers[right] > target:
+        elif sum_ > target:
             right -= 1
-        elif numbers[left] + numbers[right] > target:
+        elif sum_ < target:
             left += 1
 
 if __name__ == "__main__":
