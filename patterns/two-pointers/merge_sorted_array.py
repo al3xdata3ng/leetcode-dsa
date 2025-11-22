1ed3ce48b3a152de0a334e66bedb24ffa35a18e2
Alexandros Vitsas <al3xdata3ng@gmail.com>
2025-11-22 20:07:59 +0200

    refactor(two-pointers): merge sorted array after reading solution

--- patterns/two-pointers/merge_sorted_array.py
+++ patterns/two-pointers/merge_sorted_array.py
@@ -1,4 +1,4 @@
-def merge(nums1, m, nums2, n):
+def merge(nums1: list[int], m: int, nums2: list[int], n: int) -> None:
     """
     :type nums1: List[int]
     :type m: int
@@ -7,26 +7,30 @@ def merge(nums1, m, nums2, n):
     :rtype: None Do not return anything, modify nums1 in-place instead.
     """
     
-    res = []
-    i = 0
-    j = 0
+    p1 = m - 1
+    p2 = n - 1
+    p = m + n - 1
 
-    while i <= m - 1:
-        if nums1[i] > nums2[j]:
-            res.append(nums2[j])
-            j += 1
+    while p1 >= 0 and p2 >= 0:
+        if nums1[p1] > nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1 -= 1
         else:
-            res.append(nums1[i])
-            i += 1
-    res.extend(nums2[j:])
-
-    return res  
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        p -= 1
+    
+    while p2 >= 0:
+        nums1[p] = nums2[p2]
+        p2 -= 1
+        p -= 1
 
 if __name__ == "__main__":
 
-    nums1 = [1]
-    m = 1
-    nums2 = []
-    n = 0   
+    nums1 = [1,4,4,5,6,9,0,0,0,0,0,0]
+    m = 6
+    nums2 = [1,2,2,6,8,10]
+    n = 6   
 
-    print(merge(nums1, m, nums2, n))
+    merge(nums1, m, nums2, n)
+    print(nums1)  # Expected output: [1,1,2,2,4,4,5,6,6,8,9,10]
