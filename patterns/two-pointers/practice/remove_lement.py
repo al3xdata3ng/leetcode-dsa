7b10fff5249823fd4eafc4ed58eb01915d4b7a8c
Alexandros Vitsas <al3xdata3ng@gmail.com>
2025-11-24 00:35:11 +0200

    feat(two-pointers): remove elements solution after reading it

---
+++ patterns/two-pointers/practice/remove_lement.py
@@ -0,0 +1,22 @@
+def removeElement(nums: list[int], val: int) -> int:
+    """
+    :type nums: List[int]
+    :type val: int
+    :rtype: int
+    """
+
+    k = 0 
+
+    for i in range(len(nums)):
+
+        if nums[i] != val:
+            nums[k] = nums[i]
+            k += 1
+    return k
+
+if __name__ == "__main__":
+    nums = [3, 2, 2, 3]
+    val = 3
+    k = removeElement(nums, val)
+    print(k)  # Expected output: 2
+    print(nums[:k])  # Expected output: [2, 2]
