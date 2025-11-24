23f850c940d7ca7a6d6e850af9ddf2f3e33ad259
Alexandros Vitsas <al3xdata3ng@gmail.com>
2025-11-24 21:24:19 +0200

    feat(two-pointers): remove duplicates solution without reading solution

---
+++ patterns/two-pointers/practice/easy/remove_duplicates.py
@@ -0,0 +1,20 @@
+def removeDuplicates(nums: list[int]) -> int:
+    """
+    :type nums: List[int]
+    :rtype: int
+    """
+
+    k = 1
+
+    for i in range(len(nums) - 1):
+        if nums[i + 1] != nums[i]:
+            nums[k] = nums[i + 1]
+            k += 1
+        
+    return k
+
+if __name__ == "__main__":
+    nums = [1, 1, 2]
+    k = removeDuplicates(nums)
+    print(k)  # Expected output: 2
+    print(nums[:k])  # Expected output: [1, 2]
