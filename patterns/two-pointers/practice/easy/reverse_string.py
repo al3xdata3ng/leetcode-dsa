a843822c6bd257f32c03bae229de1f218c3b95fc
Alexandros Vitsas <al3xdata3ng@gmail.com>
2025-11-30 00:22:41 +0200

    feat(two-pointers): reverse string no cheating

---
+++ patterns/two-pointers/practice/easy/reverse_string.py
@@ -0,0 +1,19 @@
+def reverseString(s: list) -> None:
+    """
+    :type s: List[str]
+    :rtype: None Do not return anything, modify s in-place instead.
+    """
+
+    i = 0 
+    j = len(s) - 1 
+
+    while i <= j:
+        s[i], s[j] = s[j], s[i]
+
+        i += 1
+        j -= 1
+
+if __name__ == "__main__":
+    s = ["h","e","l","l","o"]
+    reverseString(s)
+    print(s)  # Output: ["o","l","l","e","h"]
