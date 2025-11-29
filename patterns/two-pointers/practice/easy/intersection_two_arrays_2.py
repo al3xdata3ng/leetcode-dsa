a3166c5cf46bc5092541929422487794867e855c
Alexandros Vitsas <al3xdata3ng@gmail.com>
2025-11-30 00:05:37 +0200

    feat(two-pointers): initial solution no cheating

---
+++ patterns/two-pointers/practice/easy/intersection_two_arrays_2.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+def intersect(nums1, nums2):
+    """
+    :type nums1: List[int]
+    :type nums2: List[int]
+    :rtype: List[int]
+    """
+
+    c1, c2 = Counter(nums1), Counter(nums2)
+    intersection = []
+
+    for i in c1.keys():
+        for j in c2.keys():
+            if i == j:
+                intersection.extend([i] * min(c1[i], c2[j]))
+    
+    return intersection
+
+
+if __name__ == "__main__":
+    arr1 = [1, 2, 2, 1]
+    arr2 = [2, 2]
+    print(intersect(arr1, arr2))  # Output: [2, 2]
